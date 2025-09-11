d18838583d1d0829ff5be9e7488c181bc12805aa
Tien Nguyen <tiennguyen.bio@gmail.com>
2025-09-11 11:01:54 +1000

    Import libraries and load data

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from kagglebank!")
+# Import libraries
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, roc_auc_score, roc_curve, auc
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from scipy.stats import chi2_contingency, ks_2samp
+from scipy.interpolate import make_interp_spline
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+import joblib
+import gzip
 
-
-if __name__ == "__main__":
-    main()
+# Load data
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
